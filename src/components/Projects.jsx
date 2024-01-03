4c90e5e8ba0d2f26fd43e1a968a5de00e33e23b9
KelvynLenis <kmartinslenis30@hotmail.com>
2024-01-03 17:04:23 -0300

    integrating with api

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProjectCard from './ProjectCard';
 
-import { projects } from '../utils/projects';
+// import { projects } from '../utils/projects';
+import { api } from '../libs/api';
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    api.get('/projects')
+      .then(res => {
+        setProjects(res.data)
+      })
+      .catch(err => console.log(err))
+      .finally(() => setIsLoading(false))
+  }, [])
+
   return (
     <section id="projects" className='w-full h-full flex flex-col items-center gap-5'>
       <h1 className='text-white text-4xl'>Projects</h1>
       <div className='w-full flex flex-wrap justify-center gap-12 text-white'>
-        {projects.map((project, index) => {
-          return (
-            <ProjectCard
-              key={index}
-              index={index} 
-              title={project.title} 
-              tags={project.tags}
-              description={project.description}
-              image={project.imgURL}
-              source_code_link={project.link}
-            />
-          )
-        })}
+        {
+          isLoading ? (
+            <h1>Loading...</h1>
+          ) : (
+            projects.map((project, index) => {
+            return (
+              <ProjectCard
+                key={index}
+                index={index} 
+                title={project.title} 
+                tags={project.tags}
+                description={project.description}
+                image={project.img_url}
+                source_code_link={project.source_code}
+                website_link={project.website_url}
+              />
+            )
+          })
+        )}
       </div>
     </section>
   )
